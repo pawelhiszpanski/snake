996e72f5cea488d8f2edbb9d80beb72550817519
Pawel <pawel.hiszpanski@gmail.com>
2026-04-18 20:55:31 +0200

    little changes

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include <time.h>
 
 extern "C" {
-//#include"./SDL2-2.0.10/include/SDL.h"dobra
+//#include"./SDL2-2.0.10/include/SDL.h"
 #include <SDL.h>
 //#include"./SDL2-2.0.10/include/SDL_main.h"
 #include <SDL_main.h>
